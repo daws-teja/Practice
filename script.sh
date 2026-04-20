e96863cf2d23a4573307dba7a60c9fb5b9987a49
teja-105 <tejachintapalli@gmail.com>
2026-04-20 11:27:27 +0530

    third commit

--- script.sh
+++ script.sh
@@ -1,5 +1,20 @@
 #!#bin/#bash
 
-echo "working"
+# install mginx only with root user
 
-echo "first commit"
+USER_ID=$(id -u)
+
+fi [ $USER_ID -ne 0 ]; then
+    echo "please run with root user access"
+    exit 1
+fi
+
+echo "Installing Nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "installing nginx.. FAILURE"
+    exit 1
+else
+    echo "installing nginx.. SUCCESS"
+fi
