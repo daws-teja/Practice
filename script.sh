376c3b31174c62155444f2fab78b2535ed0bcec2
tejachintapalli <tejachintapalli@gmail.com>
2026-04-27 15:53:06 +0530

    added

--- script.sh
+++ script.sh
@@ -1,5 +1,29 @@
 #!#bin/#bash
 
 
+echo all variabes passed to script 
+$@
 
-echo connected 
+echo number of variables psased to script 
+$#
+
+echo script name 
+$0
+
+echo present working directory 
+$PWD
+
+echo who is running the script 
+$USER
+
+echo home directory of user
+$HOME
+
+echo process id of script
+$$
+
+echo background process id 
+$!
+
+exit status of previous command
+$?
