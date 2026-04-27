7f9142619efbab9b478491a84f1862c67b654ff7
tejachintapalli <tejachintapalli@gmail.com>
2026-04-27 15:56:58 +0530

    added

--- script.sh
+++ script.sh
@@ -1,29 +1,19 @@
-#!#bin/#bash
+#!/bin/bash
 
+echo "all variables passed to script: $@"
 
-echo all variabes passed to script 
-$@
+echo "number of variables passed to script: $#"
 
-echo number of variables psased to script 
-$#
+echo "script name: $0"
 
-echo script name 
-$0
+echo "present working directory: $PWD"
 
-echo present working directory 
-$PWD
+echo "who is running the script: $USER"
 
-echo who is running the script 
-$USER
+echo "home directory of user: $HOME"
 
-echo home directory of user
-$HOME
+echo "process id of script: $$"
 
-echo process id of script
-$$
+echo "background process id: $!"
 
-echo background process id 
-$!
-
-echo exit status of previous command
-$?
+echo "exit status of previous command: $?"
