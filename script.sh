9ecdbee786e29c5a72830337ae4fc551a2790987
tejachintapalli <tejachintapalli@gmail.com>
2026-04-27 12:09:18 +0530

    another

--- script.sh
+++ script.sh
@@ -1,20 +1,5 @@
 #!#bin/#bash
 
-# install mginx only with root user
 
-USER_ID=$(id -u)
 
-if [ $USER_ID -ne 0 ]; then
-    echo "please run with root user access"
-    exit 1
-fi
-
-echo "Installing Nginx"
-dnf install nginx -y
-
-if [ $? -ne 0 ]; then
-    echo "installing nginx.. FAILURE"
-    exit 1
-else
-    echo "installing nginx.. SUCCESS"
-fi
+echo teja
