01e6ea9e47cd8c295ebd73a49f885f5db1c2bf30
tejachintapalli <tejachintapalli@gmail.com>
2026-05-02 10:01:45 +0530

    added

--- roboshop.sh
+++ roboshop.sh
@@ -1,43 +1,63 @@
 #!/bin/bash
 
+SG_ID="sg-060b4fb9a7c4378d1" 
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-060b4fb9a7c4378d1"
+ZONE_ID="Z0309799209IXS91WNLI"
+DOMAIN_NAME="tejachintapalli.online"
 
-
-for INSTANCE in "$@"
-do 
+for instance in $@
+do
     INSTANCE_ID=$( aws ec2 run-instances \
-      --image-id $AMI_ID \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-      --instance-type t3.micro \
-      --security-group-ids $SG_ID \
-      --subnet-id "subnet-0a36ee13c88519dc2" \
-      --query 'Instances[0].InstanceId' \
-      --output text
-    )
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 
-    if [ "$INSTANCE" == "frontend" ]; then
-
+    if [ $instance == "frontend" ]; then
         IP=$(
-                aws ec2 describe-instances \
-                --instance-ids $INSTANCE_ID \
-                --query 'Reservations[].Instances[].PublicIpAddress' \
-                --output text
-
-            )
-        echo "Public IP Adress OF $INSTANCE is : $IP"
-
-    else 
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" 
+    else
         IP=$(
-                aws ec2 describe-instances \
-                --instance-ids $INSTANCE_ID \
-                --query 'Reservations[].Instances[].PrivateIpAddress' \
-                --output text
-
-            )
-        echo "Private IP Adress of $INSTANCE is : $IP"
-
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
 
-done
+    echo "IP Address: $IP"
 
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
